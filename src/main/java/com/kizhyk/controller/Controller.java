799361d009bdd50e2a7d3260fc4f11696a28bae3
Kizhyk <nik.kizhyk@gmail.com>
2021-06-28 23:53:41 +0300

    controller: added main controller logic class Controller.java

---
+++ src/main/java/com/kizhyk/controller/Controller.java
@@ -0,0 +1,28 @@
+package com.kizhyk.controller;
+
+import com.kizhyk.controller.command.CommandProvider;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Controller extends HttpServlet {
+    private final CommandProvider provider = new CommandProvider();
+
+    public Controller() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String command = req.getParameter("command");
+        provider.getCommand(command).execute(req, resp);
+    }
+}
