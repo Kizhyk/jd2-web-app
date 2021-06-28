c8e5c2ca6cbcdbd7cba41974d7609cb1df504e0b
Kizhyk <nik.kizhyk@gmail.com>
2021-06-29 01:09:41 +0300

    controller: created final variables for constant strings

--- src/main/java/com/kizhyk/controller/Controller.java
+++ src/main/java/com/kizhyk/controller/Controller.java
@@ -11,18 +11,21 @@ import java.io.IOException;
 public class Controller extends HttpServlet {
     private final CommandProvider provider = new CommandProvider();
 
+    private static final String COMMAND_REQUEST_PARAM = "command";
+    private static final String INITIAL_PAGE_PATH = "/WEB-INF/jsp/index.jsp";
+
     public Controller() {
         super();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(req, resp);
+        req.getRequestDispatcher(INITIAL_PAGE_PATH).forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String command = req.getParameter("command");
+        String command = req.getParameter(COMMAND_REQUEST_PARAM);
         provider.getCommand(command).execute(req, resp);
     }
 }
