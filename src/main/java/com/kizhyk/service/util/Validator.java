dbf01d9fea187fd9e68bbd4ead5bf2f8088f8a92
Kizhyk <nik.kizhyk@gmail.com>
2021-06-29 19:31:44 +0300

    service: added Validator util class

---
+++ src/main/java/com/kizhyk/service/util/Validator.java
@@ -0,0 +1,53 @@
+package com.kizhyk.service.util;
+
+import com.kizhyk.bean.UserEntity;
+import com.kizhyk.service.exception.ServiceIllegalArgumentException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    private static final Validator instance = new Validator();
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+    private Validator() {
+    }
+
+    public static Validator getInstance() {
+        return instance;
+    }
+
+    private void validateEmail(String email) throws ServiceIllegalArgumentException {
+        Matcher m = pattern.matcher(email);
+        if (!m.matches()) {
+            throw new ServiceIllegalArgumentException();
+        }
+    }
+
+    private void validateLogin(String login) throws ServiceIllegalArgumentException {
+
+    }
+
+    private void validatePassword(String password) throws ServiceIllegalArgumentException {
+        if (password == null || password.isEmpty()) {
+            throw new ServiceIllegalArgumentException();
+        }
+
+        if (password.length() < 4 || password.length() > 20) {
+            throw new ServiceIllegalArgumentException();
+        }
+
+
+
+    }
+
+    public void validateUser(UserEntity user) throws ServiceIllegalArgumentException {
+        if (user == null) {
+            throw new ServiceIllegalArgumentException();
+        }
+        validateLogin(user.getLogin());
+        validateEmail(user.getEmail());
+        validatePassword(user.getPassword());
+    }
+}
