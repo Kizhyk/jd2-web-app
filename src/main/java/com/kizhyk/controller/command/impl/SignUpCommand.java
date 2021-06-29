1e020bcecfbb35223db64e05e47472fa9ccdaeb2
Kizhyk <nik.kizhyk@gmail.com>
2021-06-29 19:32:40 +0300

    controller: modified SignUpCommand class to call UserService method

--- src/main/java/com/kizhyk/controller/command/impl/SignUpCommand.java
+++ src/main/java/com/kizhyk/controller/command/impl/SignUpCommand.java
@@ -1,6 +1,8 @@
 package com.kizhyk.controller.command.impl;
 
 import com.kizhyk.controller.command.Command;
+import com.kizhyk.service.ServiceFactory;
+import com.kizhyk.service.exception.ServiceIllegalArgumentException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -9,7 +11,17 @@ import java.io.IOException;
 public class SignUpCommand implements Command {
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        //Service methods call
-        resp.getWriter().println(String.format("Welcome to our website, %s!", req.getParameter("login")));
+        String login = req.getParameter("login");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String message = null;
+        try {
+            ServiceFactory.getInstance().getUserService().signUp(login, email, password);
+            message = String.format("Welcome to our website, %s!", login);
+        } catch (ServiceIllegalArgumentException e) {
+            message = "Please, enter new data and try again";
+        }
+
+        resp.getWriter().println(message);
     }
 }
